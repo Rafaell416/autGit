64b364a097c7983b7de6ea779e60f8b9bf93f850
jose Gabriel <jguzmandev@gmail.com>
2017-01-26 22:05:34 -0500

    m

--- git.js
+++ git.js
@@ -1,30 +1,20 @@
 var co =  ' git add -A ; git commit -m "m" ';
 var cog =  'git add -A ; git commit -m "m" ;git push -u origin master';
-
-
 var comm = 'if netcat -z google.com 80; then echo "1"; else echo "0"; fi'
-
 var exec = require('child_process').exec, child;
 child = exec(comm,
   function (error, stdout, stderr) {
-    console.log(stdout);
-     
-    
+    //console.log(stdout);
 if(stdout  =  "1" ){
-     
-    
 var s = require('child_process').exec, child;
 child = s(co,
   function (error, stdout, stderr) {
-    console.log(stdout);    
+    //console.log(stdout);    
     if (error !== null) {
       console.log('exec error: ' + error);
     }
 });
-    
-        
-    }else{
-         
+    }else{      
 var x = require('child_process').exec, child;
 child = x(cog,
   function (error, stdout, stderr) {
@@ -32,13 +22,8 @@ child = x(cog,
     if (error !== null) {
       console.log('exec error: ' + error);
     }
-});
-
-        
+});     
     }
-         
-    
-    
     if (error !== null) {
       console.log('exec error: ' + error);
     }
