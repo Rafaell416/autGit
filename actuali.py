dc5e3b5c77896b803c40090459fa424d026c3556
jose Gabriel <jguzmandev@gmail.com>
2017-01-28 21:33:15 -0500

    k

--- actuali.py
+++ actuali.py
@@ -1,8 +1,9 @@
 import subprocess
 re="x"
-print (re)
+
 re = subprocess.call('there_are_changes=$(git status | grep nothin);echo $there_are_changes', shell=True )
-print (re)
+
+
 
 
 if re == '0':
