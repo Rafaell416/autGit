5544dc8575938f1a821c041d3860df82c083c7c5
jose Gabriel <jguzmandev@gmail.com>
2017-01-28 21:26:02 -0500

    k

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Python 
 shell script (linux) 
 git hub 
+linux
--- actuali.py
+++ actuali.py
@@ -1,11 +1,12 @@
 import subprocess
-
+re="x"
+print (re)
 re = subprocess.call('there_are_changes=$(git status | grep nothin);echo $there_are_changes', shell=True )
 print (re)
 
-if re == 0:
-	print "No Hay Cambios En El Proyecto";
-else:
-	print "Hay Cambios En El Proyecto";
 
-	
+if re == '0':
+	print "Hay Cambios En El Proyecto";
+else:
+	print "NO Hay Cambios En El Proyecto";
+
