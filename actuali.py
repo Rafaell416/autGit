7348788cf0dafcab3f3ad8cf4f1148867c5cb82f
jose Gabriel <jguzmandev@gmail.com>
2017-01-28 20:48:55 -0500

    jW

--- actuali.py
+++ actuali.py
@@ -1,5 +1,5 @@
-cadena = "unemail@ficticio.dom";
-if cadena.find("@") >= 0:
-	print "Se ha encontrado la @ en el e-mail";
-else:
-	print "No se ha encontrado la @ en el e-mail";
+import subprocess
+comm = 'if git status; then echo "1"; else echo "0"; fi'
+ret = subprocess.call( comm, shell=True)
+
+print(ret);
