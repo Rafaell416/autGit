a6abd150835568bbea7ce3b9ddc28940b34f7daa
jose Gabriel <jguzmandev@gmail.com>
2017-02-06 11:14:46 -0500

    go lang

--- git.go
+++ git.go
@@ -21,11 +21,11 @@ func leerComentario() {
     //fmt.Scanf("%s",&comentario)
     //fmt.Println("hola",comentario)
     //cmd := exec.Command("/bin/sh", mongoToCsvSH)
-     out, err := exec.Command("if netcat -z google.com 80; then echo “Tienes conexion”; else echo “no tienes conexion”; fi").Output()
+     out, err := exec.Command("echo 'hola'").Output()
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Printf("The  %s\n", out)
+    fmt.Printf( out)
 
  
 }
