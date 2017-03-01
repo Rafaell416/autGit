89db71ee494b62c0cc659102978ab7ff7ee5c6e4
jose Gabriel <jguzmandev@gmail.com>
2017-03-01 15:17:30 -0500

    compilado

---
+++ git.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/*if netcat -z google.com 80; then echo "1"; else echo "0"; fi 
+ 
+ char[1]  ping ; 
+*/
+int main(){
+
+
+  system("git add -A  ");
+  system("git commit -m compilado"); 
+  system("git push -u origin master");
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
