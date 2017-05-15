dfd79ec8563c9316e3a71a39598a995731a880bc
joseGabriel06 <jguzmandev@gmail.com>
2017-05-15 10:43:18 -0500

    python 3

--- git.py
+++ git.py
@@ -1,30 +1,36 @@
-#!/usr/local/bin/python
+
 import subprocess, time
-hosts = ('8.8.8.8', 'kernel.org', 'github.com')
+
+hosts = ('8.8.8.8','kernel.org','github.com')
+
+proceso = '''
+      echo "Comentario :"
+      read  comen 
+      git add -A
+      git commit -m  "$comen"
+ '''
+githut='git push -u origin master'
 def ping(host):
   ret = subprocess.call(['ping', '-c', '3', '-W', '5', host],
     stdout=open('/dev/null', 'w'),
     stderr=open('/dev/null', 'w'))
   return ret == 0
-def net_is_up():
-  print "[%s] Verificando Internet..." % time.strftime("%Y-%m-%d %H:%M:%S")
+
+def main():
+  print ("[%s] Verificando Internet..." % time.strftime("%Y-%m-%d %H:%M:%S"))
   xstatus = 1
-  githut='''git push -u origin master'''
-  proceso ='''
-      echo "Comentario :"
-      read  comen 
-      git add -A
-      git commit -m  "$comen"
-      '''
   for h in hosts:
     if ping(h):
       final = proceso  + githut
       subprocess.call( final, shell=True)
-      print "[%s] Se Enviara a el prepositorio Remoto!" % time.strftime("%Y-%m-%d %H:%M:%S")
+      print ("[%s] Se Enviara a el prepositorio Remoto!" % time.strftime("%Y-%m-%d %H:%M:%S"))
       xstatus = 0
       break
   if xstatus:
     subprocess.call( proceso, shell=True)
-    print "[%s] No Se Enviara Al repositorio Remoto :(" % time.strftime("%Y-%m-%d %H:%M:%S")
+    print ("[%s] No Se Enviara Al repositorio Remoto :(" % time.strftime("%Y-%m-%d %H:%M:%S"))
   return xstatus
-quit(net_is_up())
+quit(main())
+	
+if __name__ == '__main__':
+	main()
